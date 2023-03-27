82efd3b5887211c9edb20f34fa19773398b9cbcb
Dawid <solnactic@gmail.com>
2023-03-27 17:03:14 +0200

    template finished

--- template/script.js
+++ template/script.js
@@ -4,30 +4,30 @@ $("#closeSidebarBtn").on("click", () => $("body").removeClass("sidebar-open"));
 // Open / Closes dropdowns, also closes the sidebar eventually 
 $("html").on("click", (e) => {
   // Find from the clicked element the closest dropdown menu, and if so, break from the function
-  const navDropdownMenus = $(".nav__dropdown-menu");
-  if ($(e.target).closest(navDropdownMenus).length) {
+  const headerDropdownMenus = $(".header__dropdown-menu");
+  if ($(e.target).closest(headerDropdownMenus).length) {
     return;
   }
-  // Find from the clicked element the closest nav-link
-  const currentNavLink = $(e.target).closest($(".nav__link"));
-  if (currentNavLink.length) {
+  // Find from the clicked element the closest header-link
+  const currentheaderLink = $(e.target).closest($(".header__link"));
+  if (currentheaderLink.length) {
     // Close all other dropdowns, and open it's dropdown
-    navDropdownMenus.each((index, el) => {
-      el !== currentNavLink.next().get(0) &&
-        $(el).removeClass("nav__dropdown-menu--open");
+    headerDropdownMenus.each((index, el) => {
+      el !== currentheaderLink.next().get(0) &&
+        $(el).removeClass("header__dropdown-menu--open");
     });
-    currentNavLink.next().toggleClass("nav__dropdown-menu--open");
+    currentheaderLink.next().toggleClass("header__dropdown-menu--open");
   } else if (
-    // If closest is navMenu or openSidebarBtn
-    $(e.target).closest("#navMenu").length ||
+    // If closest is headerMenu or openSidebarBtn
+    $(e.target).closest("#headerMenu").length ||
     $(e.target).closest("#openSidebarBtn").length
   ) {
     // Break from the function
     return;
   } else {
     // Close all dropdowns and the sidebar if the user clicked somewhere else
-    navDropdownMenus.each((index, el) =>
-      $(el).removeClass("nav__dropdown-menu--open")
+    headerDropdownMenus.each((index, el) =>
+      $(el).removeClass("header__dropdown-menu--open")
     );
     $("body").removeClass("sidebar-open");
   }
